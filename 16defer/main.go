018fae67cfd1207bc1a20ef6362f27ac45c2bed2
Anil Kumar Vinnakoti <anil.vinnakoti@gmail.com>
2025-04-19 22:54:27 +0530

    add comments to files and defer

--- 16defer/main.go
+++ 16defer/main.go
@@ -5,15 +5,24 @@ import (
 )
 
 func main() {
+	// Tip: defer statements are executed in **reverse order (LIFO)** when the function exits
 	defer fmt.Println("world")
 	defer fmt.Println("one")
 	defer fmt.Println("two")
-	fmt.Println("hello")
+
+	fmt.Println("hello") // This prints first
+
 	myDefer()
 }
 
 func myDefer() {
+	// Tip: Each deferred call captures the value of 'i' at the time it is deferred
 	for i := 0; i < 4; i++ {
 		defer fmt.Println(i)
 	}
+	// Output will be:
+	// 3
+	// 2
+	// 1
+	// 0
 }
--- 17files/main.go
+++ 17files/main.go
@@ -3,37 +3,55 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"os" // Tip: os package handles file system operations
 )
 
 func main() {
 	fmt.Println("welcome to files")
 
+	// Content to be written to the file
 	content := "this is a dummy text."
 
+	// ------------------------------------------
+	// Create a new file (or truncate if it exists)
+	// Tip: os.Create creates or overwrites the file
+	// ------------------------------------------
 	file, osErr := os.Create("./myFile.txt")
-
 	checkNilError(osErr)
-	length, writeErr := io.WriteString(file, content)
 
+	// ------------------------------------------
+	// Write content to the file using io.WriteString
+	// Tip: io.WriteString returns number of bytes written and error
+	// ------------------------------------------
+	length, writeErr := io.WriteString(file, content)
 	checkNilError(writeErr)
 	fmt.Println("length is:", length)
 
+	// ------------------------------------------
+	// Tip: Always defer file.Close() to release the file resource
+	// ------------------------------------------
 	defer file.Close()
 
+	// Read and print file content
 	readFile("./myFile.txt")
-
 }
 
+// ------------------------------------------
+// Read a file and print its content
+// Tip: os.ReadFile reads the full file content into memory
+// ------------------------------------------
 func readFile(fileName string) {
 	dataByte, osErr := os.ReadFile(fileName)
 	if osErr != nil {
 		panic(osErr)
 	}
 	fmt.Println("txt data inside file is:", string(dataByte))
-
 }
 
+// ------------------------------------------
+// Centralized error handling
+// Tip: A helper function to panic if error is non-nil
+// ------------------------------------------
 func checkNilError(err error) {
 	if err != nil {
 		panic(err)
